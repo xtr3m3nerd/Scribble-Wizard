7c8a6be36519073338279cfaf4c91d6ec5afe499
Xavier Vargas <xtr3m3nerd@gmail.com>
2024-04-22 17:22:05 -0400

    Make level generator dynamic and configurable

--- scenes/game/game_manager.gd
+++ scenes/game/game_manager.gd
@@ -2,13 +2,28 @@ class_name GameManager
 extends Node
 
 signal level_finished
+signal game_win
 signal kills_changed(kills: int)
 
+@export var map_generator: MapGenerator
+
+var current_level = -1
+@export var levels: Array[DungeonLevel] = []
+
 var kills: int = 0
+
+func _ready():
+	start_next_level()
 
 func add_kill():
 	kills += 1
 	kills_changed.emit(kills)
 
 func start_next_level():
-	print("Next Level")
+	current_level += 1
+	if current_level < levels.size():
+		print("Next Level")
+		map_generator.generate_level(levels[current_level])
+	else:
+		print("You Win")
+		game_win.emit()
