2ec854a6202b3df2355bf1008ddc6e47e073f396
Robert Claus <robertclaus@gmail.com>
2024-04-23 17:08:52 -0500

    Classifier Model

--- server/training/convert_to_onnx.py
+++ server/training/convert_to_onnx.py
@@ -3,4 +3,3 @@ from ultralytics import YOLO
 # Load a YOLOv8n PyTorch model
 dart_model = YOLO('model.pt')
 dart_model.export(format='onnx')
-
--- server/training/generate_training_set.py
+++ server/training/generate_training_set.py
@@ -5,24 +5,17 @@ import numpy as np
 import cv2
 from PIL import Image 
 
-classes = ["lightning", "fire", "water"]
+classes = ["lightning", "fire", "water", "bad"]
 categories = ["train", "train", "train", "train", "train", "valid", "test"]
 
-os.makedirs("training/data/test/images", exist_ok=True)
-os.makedirs("training/data/test/labels", exist_ok=True)
-os.makedirs("training/data/train/images", exist_ok=True)
-os.makedirs("training/data/train/labels", exist_ok=True)
-os.makedirs("training/data/valid/images", exist_ok=True)
-os.makedirs("training/data/valid/labels", exist_ok=True)
-
-def resize_img(filename):
+def resize_img(filename, box):
     img = Image.open(filename)
+    img = img.crop(box)
     img = img.resize((640, 640), Image.Resampling.LANCZOS)
     img.save(filename)
 
 def find_content(filename):
     img = cv2.imread(filename) # image file
-    height, width, channels = img.shape
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Threshold the image so that black text is white
     gray = 255*(gray < 128).astype(np.uint8)
@@ -31,15 +24,15 @@ def find_content(filename):
     gray_morph = cv2.morphologyEx(gray, cv2.MORPH_OPEN, O)
     # Continue where we left off
     coords = cv2.findNonZero(gray_morph) # Find all non-zero points (text)
-    x, y, w, h = cv2.boundingRect(coords) # Find minimum spanning bounding box
-    return f"{(x+(w/2))/width} {(y+(h/2))/height} {w/width} {h/height}"
+    xywh = cv2.boundingRect(coords) # Find minimum spanning bounding box
+    xyxy = (xywh[0], xywh[1], xywh[0] + xywh[2], xywh[1] + xywh[3])
+    return xyxy
 
 for (c_idx, c) in enumerate(classes):
     folder = f"images/{c}"
     for filename in os.listdir(folder):
         cat = random.choice(categories)
-        shutil.copyfile(f"{folder}/{filename}", f"training/data/{cat}/images/{c_idx}_{filename}")
-        resize_img(f"training/data/{cat}/images/{c_idx}_{filename}")
-
-        with open(f"training/data/{cat}/labels/{c_idx}_{filename[:-4]}.txt", "w") as file:
-            file.write(f"{c_idx} {find_content(f'{folder}/{filename}')}")
+        os.makedirs(f"training/data/{cat}/{c}", exist_ok=True)
+        shutil.copyfile(f"{folder}/{filename}", f"training/data/{cat}/{c}/{c_idx}_{filename}")
+        bounding_box = find_content(f'{folder}/{filename}')
+        resize_img(f"training/data/{cat}/{c}/{c_idx}_{filename}", bounding_box)
